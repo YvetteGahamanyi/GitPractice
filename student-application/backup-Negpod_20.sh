173df9aef6f31da33520ecb8e0f37cd35b5c305f
YvetteGahamanyi <ygahamanyi26@gmail.com>
2024-02-19 11:44:16 -0800

    backup

---
+++ student-application/backup-Negpod_20.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#configuration 
+
+HOST="64293e56bc62.3a2627c1.alu-cod.online"
+USERNAME="64293e56bc62"
+PASSWORD="328d3b338a4ced526c9a"
+DESTINATION="/summative/1023-2024j"
+
+DIRECTORY="negpod_20-q1"
+
+if [ ! -d "$DIRECTORY" ]; then
+    echo "Error: Local directory '$DIRECTORY' does not exist."
+    exit 1
+fi
+
+#Backup command
+rsync -avz -e "sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no" "$DIRECTORY" "$USERNAME@$HOST:$DESTINATION"
+
+# Check rsync exit status
+if [ $? -eq 0 ]; then
+    echo "Backup completed successfully."
+else
+    echo "Error: Backup failed."
+fi
+
+
